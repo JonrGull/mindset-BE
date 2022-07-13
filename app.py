1941d774115a58335437310f374ae0e0d6fb841e
yccjs <yccjsalvador@gmail.com>
2022-07-13 17:04:03 +0900

    chore: install dependencies for db

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ app = Flask(__name__)
 def hello_world():
     return 'Hi there!'
 
+@app.route("/login") 
+def login():
+    return "this is login"
 
 if __name__ == '__main__':
     app.run('0.0.0.0', 8080, debug=True)
